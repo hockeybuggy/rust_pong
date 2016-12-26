688f13fb7edda976d48b503d7b5ee3611c6e6af5
Douglas Anderson <hockeybuggy@gmail.com>
2016-12-25 23:34:35 -0500

    Animate dat. The red triangle now moved. Like a boss.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,76 @@
 #[macro_use]
 extern crate glium;
 
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+
+implement_vertex!(Vertex, position);
+
+
+
 fn main() {
     use glium::{DisplayBuild, Surface};
 
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+
+        uniform float t;
+
+        void main() {
+            vec2 pos = position;
+            pos.x += t;
+            gl_Position = vec4(pos, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+
+        out vec4 color;
+
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
     let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
 
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5] };
+    let vertex3 = Vertex { position: [ 0.5,  0.25] };
+
+    let mut t: f32 = -0.5;
+
+    let shape = vec![vertex1, vertex2, vertex3];
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
     loop {
+        t += 0.0002;
+        if t > 0.5 {
+            t = -0.5;
+        }
+        // println!("{}", t);
+
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
+
+        target.draw(
+            &vertex_buffer,
+            &indices,
+            &program,
+            &uniform! { t: t },
+            &Default::default()
+        ).unwrap();
+
         target.finish().unwrap();
 
         for event in display.poll_events() {
